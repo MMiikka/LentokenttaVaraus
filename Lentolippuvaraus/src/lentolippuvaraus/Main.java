d5cae3de663c2df52f446b99e9c16ff88bef910a
MMiikka <jmmerilahti@gmail.com>
2021-04-23 17:06:13 +0300

    Projektin lisäys Gitiin

---
+++ Lentolippuvaraus/src/lentolippuvaraus/Main.java
@@ -0,0 +1,18 @@
+package lentolippuvaraus;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Ohjelma m = new Ohjelma();
+	
+		m.paavalikko();
+	
+	}
+}
